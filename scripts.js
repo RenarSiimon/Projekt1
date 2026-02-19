245d6e1f05aac51f05f67d2db40579fab565a9b6
renar siimon <Renar.Siimon@tpt.lan>
2026-02-19 12:42:07 +0200

    joke

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
-const jokeBox = document.getElementById("joke");
-const jokeBtn = document.getElementById("JokeBtn");
+let jokeText = document.getElementById("joke");
+let getJokeButton = document.getElementById("JokeBtn");
+let showPunchlineButton = document.getElementById("Punchline");
+let savedPunchline = "";
 
-async function getJoke() {
-  try {
-    const response = await fetch("https://official-joke-api.appspot.com/random_joke");
-    const data = await response.json();
-    jokeBox.textContent = data.value; 
+async function getJoke() 
+{
+    let response = await fetch("https://official-joke-api.appspot.com/random_joke");
+    let jokeData = await response.json();
+    jokeText.textContent = jokeData.setup;
+    savedPunchline = jokeData.punchline;
+    showPunchlineButton.disabled = false;
 }
-
-jokeBtn.addEventListener("click", getJoke);
+function showPunchline() 
+{
+    jokeText.textContent = jokeText.textContent + "\n\n" + savedPunchline;
+    showPunchlineButton.disabled = true;
+}
+getJokeButton.addEventListener("click", getJoke);
+showPunchlineButton.addEventListener("click", showPunchline);
